8efb86ff437488a5b4d3255775ec1deef26bf3f9
Murali0070 <muralimanohardasari@gmail.com>
2026-06-16 15:17:27 +0530

    Rename folder to lowercase

--- e-commerce/src/components/FormalShoesCard.jsx
+++ e-commerce/src/components/FormalShoesCard.jsx
@@ -1,7 +1,7 @@
 ﻿import React, { useContext, useState } from "react";
 import { CartContext } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
-import nikeImage from "../assets1/nikeimage.png";
+import nikeImage from "../Assets1/nikeImage.png";
 
 function FormalShoesCard({ shoe }) {
     const { addToCart } = useContext(CartContext);
